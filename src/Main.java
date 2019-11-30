c12c1b2dee1a6e0fe503d096d31ce43f7ee0a6ee
xietenggang <qinghuai22@163.com>
2019-11-30 17:04:58 +0800

    版本3.0

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("版本2.0");
+        System.out.println("版本3.0");
 
     }
 }
